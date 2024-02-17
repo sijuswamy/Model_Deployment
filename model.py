56aecc4a1a006c3d1c5a84f765aa3815cab3b518
GNadmin <ganithanetra@gmail.com>
2024-02-18 01:43:16 +0530

    change data source

--- model.py
+++ model.py
@@ -8,7 +8,7 @@ import joblib
 seed = 42
 
 # Read original dataset
-iris_df = pd.read_csv("https://raw.githubusercontent.com/sijuswamy/Intel-Unnati-sessions/main/iris.csv")
+iris_df = pd.read_csv("data/iris.csv")
 iris_df.sample(frac=1, random_state=seed)
 
 # selecting features and target data
